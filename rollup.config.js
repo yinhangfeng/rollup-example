ccd7484f97c53a8e3a4da0625acf3fe82064af45
yinhf <darkwander.y@gmail.com>
2017-11-17 14:33:55 +0800

    output.file

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ const resolve = require('rollup-plugin-node-resolve');
 module.exports = {
   input: 'src/index.js',
   output: {
-    dest: 'dist/rollup-bundle.cjs.js',
+    file: 'dist/rollup-bundle.cjs.js',
     format: 'cjs'
   },
   plugins: [
